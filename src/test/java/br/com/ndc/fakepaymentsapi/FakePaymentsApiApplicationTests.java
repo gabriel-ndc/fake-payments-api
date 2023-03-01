4f9c163559eacc2ec75a042f2844fdd44e084d4b
Gabriel L. Rodrigues <gabriel.rodrigues@ndc.com.br>
2023-03-01 10:26:46 -0300

    Rename application to fake-payments-api

--- src/test/java/br/com/ndc/fakepaymentsapi/FakePaymentsApiApplicationTests.java
+++ src/test/java/br/com/ndc/fakepaymentsapi/FakePaymentsApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PaymentApiTestApplicationTests {
+class FakePaymentsApiApplicationTests {
 
 	@Test
 	void contextLoads() {
