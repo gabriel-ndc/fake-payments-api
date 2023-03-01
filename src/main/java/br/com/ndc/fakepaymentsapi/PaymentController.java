9b062025c572965c2f9db8cd19a9a08fd5777fa8
Gabriel L. Rodrigues <gabriel.rodrigues@ndc.com.br>
2023-03-01 10:34:32 -0300

    Add logging to executePayment endpoint

--- src/main/java/br/com/ndc/fakepaymentsapi/PaymentController.java
+++ src/main/java/br/com/ndc/fakepaymentsapi/PaymentController.java
@@ -2,7 +2,9 @@ package br.com.ndc.fakepaymentsapi;
 
 import br.com.ndc.fakepaymentsapi.dto.ExecutePaymentRequestDTO;
 import br.com.ndc.fakepaymentsapi.dto.ExecutePaymentResponseDTO;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,15 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Locale;
 
+@RequiredArgsConstructor
 @RestController
 public class PaymentController {
 
-    @Autowired
-    private FakePaymentService fakePaymentService;
+    private final Logger logger = LoggerFactory.getLogger(PaymentController.class);
+
+    private final FakePaymentService fakePaymentService;
 
     @PostMapping("/api/execute-payment")
     public ResponseEntity<ExecutePaymentResponseDTO> executePayment(
             @RequestBody ExecutePaymentRequestDTO executePaymentRequestDTO) {
+        logger.info("POST /execute-payment - id {}", executePaymentRequestDTO.paymentId());
+
         var paymentStatus = fakePaymentService.executePayment();
 
         var response = new ExecutePaymentResponseDTO(
